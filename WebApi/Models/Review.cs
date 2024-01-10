afaff57a1e4b7247df3e5ba0d2db52cc3b7bf6a3
mlhsn <mahmudul.hasan@monstar-lab.com>
2024-01-10 22:10:05 +0600

    feat: added rating to review

--- WebApi/Models/Review.cs
+++ WebApi/Models/Review.cs
@@ -2,13 +2,15 @@ namespace WebApi.Models;
 
 public class Review
 {
-    public uint Id { get; set; } 
-    
+    public uint Id { get; set; }
+
     public string Title { get; set; } = String.Empty;
-    
+
     public string Text { get; set; } = String.Empty;
 
     public Reviewer Reviewer { get; set; }
 
     public Pokemon Pokemon { get; set; }
+
+    public int Rating { get; set; }
 }
