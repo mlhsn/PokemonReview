bbb49776f1ca5d09fc7d2790313ac7bff427d706
mlhsn <mahmudul.hasan@monstar-lab.com>
2024-01-10 22:08:21 +0600

    feat: added db seeder

--- WebApi/Models/Owner.cs
+++ WebApi/Models/Owner.cs
@@ -3,12 +3,16 @@ namespace WebApi.Models;
 public class Owner
 {
     public uint Id { get; set; }
-    
+
     public string Name { get; set; } = String.Empty;
-    
+
     public string Gym { get; set; } = String.Empty;
 
     public Country Country { get; set; }
 
     public ICollection<PokemonOwner> PokemonOwners { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
 }
